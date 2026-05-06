75a56dacd11baa3b2cfbad81f7e1d0fc95131ee9
Laksana-Sriharan <laksana.sriharan@gmail.com>
2026-05-06 14:50:01 +0530

    Add Python Functions

---
+++ functions.py
@@ -0,0 +1,21 @@
+#Function
+
+#Task 1
+def say_hello():
+    print("Hello Dear User")
+
+say_hello()
+
+#Task 2
+def say_hello_new(name):
+    print("Hello Dear User " + name)
+
+say_hello_new("Andrew")
+say_hello_new("Aldrick")
+
+#Task 3
+def say_hello_up(name,age):
+    print("Hello Dear User " + name + ", You are " + str(age) + " years old.")
+
+say_hello_up("Andrew", 23)
+say_hello_up("Aldrick", 34)
