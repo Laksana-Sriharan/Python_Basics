030c554b97c536148a15d97c167364ba4ec48908
Laksana-Sriharan <laksana.sriharan@gmail.com>
2026-05-06 14:29:35 +0530

    Update tuples in Python

---
+++ tuple.py
@@ -0,0 +1,17 @@
+#Tuples
+
+numbers = (8,10)
+print(numbers)
+
+#To access the elements in the tuple
+print(numbers[0])
+
+#To modify the elements in the tuple
+
+#We cannot modify elements in a tuple directly like lists
+# numbers[0] = 12 will lead to an error
+
+#In a list we can have multiple tuples
+number_list = [(8,10),(88,92),(77,72)]
+print(number_list[1])
+print(number_list[-2])
